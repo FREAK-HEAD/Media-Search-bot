40295fb3f17721f05d5639ac4fa5827b4bcb6f3f
Faris <81860288+FREAK-HEAD@users.noreply.github.com>
2021-06-16 12:26:02 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -4,7 +4,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import InviteHashExpired, UserAlreadyParticipant
+from config import Config
 import re
+from bot import Bot
 from asyncio.exceptions import TimeoutError
 from database import save_data
 import logging
